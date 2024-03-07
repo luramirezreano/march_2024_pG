bf1cf3325920542bac10cdfc537ac57334e8ae9d
lucia <ramirezlucia22@gmail.com>
2024-03-06 21:39:56 -0500

    challenge Operators

---
+++ modules_PG/CLASS2/challengeOperators.js
@@ -0,0 +1,49 @@
+//Arithmetic Operators: 
+// let num1 = 10;
+// let num2 = 5;
+// let sum = (num1 + num2);
+// console.log (sum);
+
+// const pi = 3.14159; 
+//let radius = 21;
+// let area = pi * (radius ** 2);
+// console.log(area);
+
+// Assigment Operations 
+// let num = 5;
+// num+= 2;
+// console.log(num);
+
+// price = 10;
+// price*= 2.5;
+// console.log(price);
+
+//Comparison Operators 
+// let age1 =18;
+// let age2 = 17;
+// console.log(age1 > age2);
+//Number 2 
+// let name1 = 'Lucia'
+// let name2 = 'John Doe'
+// console.log (name1 != name2);
+
+//Logical Operators:
+// let isMember = 'true';
+// let hasCoupon = 'true';
+// if(isMember || hasCoupon){
+// console.log ('Eligable for Free Shipping')
+// }
+//Number 2 
+// let num1 = 10;
+//  if(!(num1 = 0)){
+//    console.log("false");
+//  }
+
+//Conditional (Ternary) Operators: 
+// let age = 18;
+// let isAdult = age >= 18 ?'You are old enough to vote' : 'You are not old enough to vote';
+// console.log(isAdult);
+//Numer 2
+// let priceOfProduct = 20;
+// let message = priceOfProduct < 15 ? 'the product is on sale' : 'The product is not on sale'
+// console.log(message);
