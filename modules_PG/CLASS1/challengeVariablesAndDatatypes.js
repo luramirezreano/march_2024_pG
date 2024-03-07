b7b5afd67b68588a27ca67130961b6c25fc6da38
lucia <ramirezlucia22@gmail.com>
2024-03-06 21:32:07 -0500

    1st challenge

---
+++ modules_PG/CLASS1/challengeVariablesAndDatatypes.js
@@ -0,0 +1,22 @@
+//# 1
+// let firstName = "Lucia";
+// console.log (firstName);
+
+//#2
+// let myAge = 31;
+// console.log(myAge);
+
+//#3
+// let isStudent = 'true'
+// console.log(isStudent);
+
+//#4
+// let favoriteColors = ["pink", "green", "purple"];
+// console.log(favoriteColors);
+
+// let person = {
+//     name: "Lucia",
+//     age: 31,
+//     isStudent:true
+// }
+// console.log(person);
