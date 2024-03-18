6f3f6e847c3b44660a7930447cd2d692f21a45ad
luramirezreano <159116525+luramirezreano@users.noreply.github.com>
2024-03-18 18:37:47 -0400

    stringChallenge

---
+++ CLASS1/challengeString.js
@@ -0,0 +1,10 @@
+//code challenge:
+let coder = 'I am a good programmer';
+//console.log(typeof 'coder');
+//console.log(coder.length);
+//console.log (coder.toUpperCase());
+//console.log(coder.toLowerCase());
+//console.log(coder.includes('good'));
+//console.log(coder.replace ('programmer', ''));
+//console.log(coder.endsWith('programmer'));
+console.log(coder.split('good'));
