044dfc6474a1897e20c81048924b959a3a898bd2
luramirezreano <159116525+luramirezreano@users.noreply.github.com>
2024-03-25 12:58:17 -0400

    Array Code Challenge

---
+++ CLASS1/arrayCodeChallenge.js
@@ -0,0 +1,50 @@
+let names = ['Ali', 'Ben', 'Cece', 'Donna', 'Kate'];
+
+//length
+//console.log(names.length);
+
+//pop
+// console.log(names);
+// let poppedValue = names.pop();
+// console.log(poppedValue);
+// console.log(names)
+
+//push
+// console.log(names);
+// let poppedValue = names.push ('Lucia');
+// console.log(poppedValue);
+// console.log(names);
+
+//shift
+// console.log(names);
+// let shiftedValue = names.shift();
+// console.log(shiftedValue);
+// console.log(names);
+
+// //unshift
+// console.log(names);
+// let unshift = names.unshift('Zoe');
+// console.log(unshift);
+// console.log(names);
+
+// //splice, adding 'Madison' after Index 2. 
+// let newNames = names.splice(2, 1, 'Madison');
+// console.log(newNames);
+// console.log(names); 
+
+//Find if array includes the value 'John'
+//console.log(names.includes('John'));
+
+// //Find index 'kate'
+// let indexOfKate = names.indexOf('Kate');
+// //console.log(indexOfKate);
+
+//reverse your array and print last index value
+// console.log (names);
+// names.reverse();
+// console.log(names);
+
+//find out if the first index value equals to last index value (hint use comparision operator. Not Loop)
+
+//let firstEqualtoLast = names [0] === names [4];
+//console.log(firstEqualtoLast);
